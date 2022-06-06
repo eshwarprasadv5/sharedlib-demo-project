c02315443966f1c593370cfb27a0eca51a874d6a
eshwarprasadv5 <83694502+eshwarprasadv5@users.noreply.github.com>
2022-06-06 22:19:20 +0530

    Create sample2.groovy

---
+++ vars/sample2.groovy
@@ -0,0 +1,13 @@
+pipeline{
+  agent{
+    label 'maven2'
+  }
+  stages{
+   stage('Checkout'){
+     steps{
+     git 'https://github.com/eshwarprasadv5/sharedlib-demo-project.git'
+     }
+    
+   }
+  }
+}
